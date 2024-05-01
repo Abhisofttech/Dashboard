b386f419275792fd86169cc5705b08a6a162d865
Abhidofttech <abhisofttech360@gmail.com>
2024-05-02 01:20:23 +0530

    resolved footer error

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import '../index.css'
+import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
    <>
    <div className="container-fluid footer d-flex align-center justify-content-between mt-5">
     <p>Copyright © Dummy Website 2023</p>
     <p className='text-grey'>
-        <a href="#">Privacy Policy</a> . 
-        <a href="#">Terms & Conditions</a>
+        <Link to='/' >Privacy Policy</Link> . 
+        <Link to='/' >Terms & Conditions</Link>
     </p>
    </div>
    </>
